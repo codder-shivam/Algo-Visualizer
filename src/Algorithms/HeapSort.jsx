d64a60ca467808ffcc300d88fe982738015fddd1
hermit_crab <99shivammishra99@gmail.com>
2020-08-02 18:54:19 +0530

    added plain heap sort algo (animations pending)

--- src/Algorithms/HeapSort.jsx
+++ src/Algorithms/HeapSort.jsx
@@ -3,5 +3,46 @@
 // const SPEED_MS = 5;
 
 export default (originalArr) => {
-	return null;
+	const animations = getAnimations(originalArr);
+	console.log(animations);
 };
+
+function getAnimations(arr) {
+	if (arr.length <= 1) return arr;
+	const animations = [];
+	heapSort(arr, animations, arr.length);
+	return animations;
+}
+
+function heapSort(mainArr, animations, n) {
+	let lastParentIdx = Math.floor((n - 1) / 2);
+	let lastHeapNodeIdx = n - 1;
+
+	for (let i = lastParentIdx; i >= 0; i++) {
+		heapify(mainArr, animations, i, n);
+	}
+
+	for (; lastHeapNodeIdx > 0; lastHeapNodeIdx--) {
+		const temp = mainArr[lastHeapNodeIdx];
+		mainArr[lastHeapNodeIdx] = mainArr[0];
+		mainArr[0] = temp;
+		heapify(mainArr, animations, 0, lastHeapNodeIdx);
+	}
+}
+
+function heapify(mainArr, animations, parentIdx, size) {
+	if (2 * parentIdx + 1 >= size) return;
+	const leftChildIdx = 2 * parentIdx + 1;
+	const rightChildIdx = leftChildIdx + 1;
+
+	let greater = parentIdx;
+	if (mainArr[leftChildIdx] > mainArr[greater]) greater = leftChildIdx;
+	if (mainArr[rightChildIdx] > mainArr[greater]) greater = rightChildIdx;
+
+	if (greater !== parentIdx) {
+		const tmp = mainArr[greater];
+		mainArr[greater] = mainArr[parentIdx];
+		mainArr[parentIdx] = tmp;
+	}
+	heapify(mainArr, animations, greater, size);
+}
